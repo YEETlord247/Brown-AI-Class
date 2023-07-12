f64d938759972970454f13be3dcc4ed1329077f7
Utkarsh <rai.utkarsh2007@gmail.com>
2023-07-11 21:37:22 -0400

    This is my submission for Task 2 for 7/11/2023.

---
+++ Task_2.py
@@ -0,0 +1,9 @@
+choice = eval(input("Hours to Minutes (Enter 1) \nMinutes to Hours (Enter 2)"))
+if (choice == 1):
+    num = eval(input("How Many Hours?"))
+    print ((num * 60), " minutes.")
+elif(choice == 2):
+    num = eval(input("How Many Minutes?"))
+    print ((num // 60), " hours.")
+else:
+    print ("Please enter 1 or 2")
